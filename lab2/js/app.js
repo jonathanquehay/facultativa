55214204cbb709c9c72334bc442e549e68183c8d
JONATHAN MORENO <jonathanmoreno@MacBook-Pro-de-JONATHAN.local>
2017-08-16 09:02:29 -0600

    Realizar operaciones matemat

--- lab2/js/app.js
+++ lab2/js/app.js
@@ -1,10 +1,16 @@
 var dato1 = document.getElementById("dato1");
 var dato2 = document.getElementById("dato2");
 var suma = document.getElementById("suma"); //boton de suma - evento click
+var resta = document.getElementById("resta"); //boton de suma - evento click
+var multiplica = document.getElementById("multiplicacion"); //boton de suma - evento click
+var divide = document.getElementById("division"); //boton de suma - evento click
 var resul = document.getElementById("resul");
 var resultado;
 
 suma.addEventListener("click",sumar);
+resta.addEventListener("click",restar);
+multiplica.addEventListener("click",multiplicar);
+divide.addEventListener("click",dividir);
 
 
 
@@ -16,7 +22,27 @@ function sumar() {
     dato1.focus();
 }
 
-
+function restar() {
+    resultado = Number(dato1.value) - Number(dato2.value);
+    resul.innerHTML = resultado;
+    dato1.value = "";
+    dato2.value = "";
+    dato1.focus();
+}
+function multiplicar() {
+    resultado = Number(dato1.value) * Number(dato2.value);
+    resul.innerHTML = resultado;
+    dato1.value = "";
+    dato2.value = "";
+    dato1.focus();
+}
+function dividir() {
+    resultado = Number(dato1.value) / Number(dato2.value);
+    resul.innerHTML = resultado.toFixed;
+    dato1.value = "";
+    dato2.value = "";
+    dato1.focus();
+}
 
 
 
